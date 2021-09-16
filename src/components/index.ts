24e57c0ec1a930368cec49b077b55572db0908a6
Saurav Kumar <maxsaurav237@gmail.com>
2021-09-16 22:46:38 +0530

    added all components to export

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { Navbar } from "./Navbar";
+export { QuestionCard } from "./QuestionCard";
+export { LogInCard } from "./LogInCard";
+export { ProfileCard } from "./ProfileCard";
+export { QuizList } from "./QuizList";
+export { QuizDetail } from "./QuizDetailModal";
